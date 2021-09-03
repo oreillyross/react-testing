45bd70033a12211bdacb8c1e83aa5775ff8f1b96
Ross O'Reilly <oreillyross@gmail.com>
2021-09-03 14:30:26 +0200

    :green_heart: palindrme working

--- test-driven-palindromes/__tests__/palindrome.test.js
+++ test-driven-palindromes/__tests__/palindrome.test.js
@@ -3,6 +3,24 @@ const palindromes = require("../palindrome");
 describe("palindromes", () => {
   test("correctly identifies one palindrome", () => {
     expect(palindromes("madam")).toEqual(["madam"]);
-    expect(palindromes("racecar")).toEqual(["racecar"])
+    expect(palindromes("racecar")).toEqual(["racecar"]);
   });
+
+  test("returns an empty array when no palindromes found", () => {
+    expect(palindromes("tic tac toe")).toEqual([])
+  })
+
+  test("it ignores casing", () => {
+    expect(palindromes('WoW')).toEqual(["wow"])
+  })
+
+  test("Ignores punctuation", () => {
+    expect(palindromes("yo, banana boy!")).toEqual(["yobananaboy"])
+  })
+
+  test("detects multi-word palindromes", () => {
+    expect(palindromes("A man, a plan, a canal, Panama")).toEqual(["amanaplanacanalpanama"])
+
+  })
+
 });
--- test-driven-palindromes/palindrome.js
+++ test-driven-palindromes/palindrome.js
@@ -1,3 +1,13 @@
-module.exports = str => {
-	return [str]
-}
+module.exports = (str) => {
+  const cleanLine = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s/g, "")
+    .toLowerCase();
+  const reverseString = cleanLine
+    .split("")
+    .reverse()
+    .join("");
+  if (cleanLine === reverseString) {
+    return [reverseString];
+  } else return [];
+};
